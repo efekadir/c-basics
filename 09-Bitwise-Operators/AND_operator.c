4cd5c60a28e6b8c684c802a0dd20e7f4aeaa3eef
Efe Kadir Küçük <efekadirkucuk@gmail.com>
2026-07-22 23:52:41 +0300

    Create AND_operator.c

---
+++ 09-Bitwise-Operators/AND_operator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    unsigned int n1, n2, i, writer = 1u << 31;
+
+    printf("Enter first number: ");
+    scanf("%u", &n1);
+
+    printf("Enter second number: ");
+    scanf("%u", &n2);
+
+    unsigned int result = n1 & n2;
+
+    printf("Result in binary: ");
+    for(i = 0; i < 32; i++){
+        printf("%c", ((result & writer) ? '1' : '0'));
+        writer >>= 1;
+    }
+    printf("\nResult in decimal: %d", result);
+}
