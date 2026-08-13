0b6408eb39c6e13cf01ea5e34aa808066cf70b61
Efe Kadir Küçük <efekadirkucuk@gmail.com>
2026-08-13 23:39:32 +0300

    Add Left_shifting.c for bitwise left shift operation
    Implement left shifting of an unsigned integer with user input.

---
+++ 09-Bitwise-Operators/Left_shifting.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    unsigned int n1, n2, i, writer = 1u << 31;
+
+    printf("Enter first number to shift: ");
+    scanf("%u", &n1);
+
+    printf("Enter the number of bits to shift: ");
+    scanf("%u", &n2);
+
+    if(n2 >= 32){
+        printf("Enter a number less than 32");
+        return 0;
+    }
+
+    unsigned int answer = n1 << n2;
+
+    for(i = 0; i < 32; i++){
+        printf("%c", ((answer & writer) ? '1' : '0'));
+        writer >>= 1;
+    }
+
+    return 0;
+}
